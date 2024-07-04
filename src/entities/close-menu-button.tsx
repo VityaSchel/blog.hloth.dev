33cac8f43209ed5b7622f37bd9189e91890cb416
VityaSchel <59040542+VityaSchel@users.noreply.github.com>
2024-07-04 21:07:43 +0400

    Add menu

---
+++ src/entities/close-menu-button.tsx
@@ -0,0 +1,38 @@
+import React from 'react'
+import cx from 'classnames'
+
+export function CloseMenuButton({ onClose }: {
+  onClose: () => void
+}) {
+  const [hover, setHover] = React.useState(false)
+  const [closing, setClosing] = React.useState(false)
+
+  const handleClose = () => {
+    setClosing(true)
+    onClose()
+  }
+
+  return (
+    <button 
+      className='flex items-center'
+      onClick={handleClose}
+      onMouseEnter={() => {
+        setHover(true)
+        setClosing(false)
+      }}
+      onMouseLeave={() => setHover(false)}
+    >
+      <span className={cx('h-[2px] bg-white transition-all', {
+        'w-0': !hover,
+        'w-[270px]': hover,
+        'duration-[390ms]': closing,
+        'duration-200': !closing
+      })} />
+      <svg width='50' height='50' viewBox='0 0 100 100' xmlns='http://www.w3.org/2000/svg'>
+        <line x1='0' y1='50' x2='90' y2='50' stroke='currentColor' stroke-width='4' />
+        <line x1='70' y1='30' x2='90' y2='51' stroke='currentColor' stroke-width='4' />
+        <line x1='70' y1='70' x2='90' y2='49' stroke='currentColor' stroke-width='4' />
+      </svg>
+    </button>
+  )
+}
--- src/features/menu.tsx
+++ src/features/menu.tsx
@@ -1,11 +1,63 @@
+import React from 'react'
 import { useTranslation } from 'next-i18next'
+import cx from 'classnames'
+import Link from 'next/link'
+import { useHotkeys } from 'react-hotkeys-hook'
+import { CloseMenuButton } from '@/entities/close-menu-button'
+import FocusTrap from 'focus-trap-react'
 
 export function Menu() {
   const { t } = useTranslation('common')
+  const [open, setOpen] = React.useState(false)
+  useHotkeys('esc', () => open && setOpen(false), [open])
+
+  React.useEffect(() => {
+    if(open) {
+      document.body.classList.add('menu-open')
+    } else {
+      document.body.classList.remove('menu-open')
+    }
+  }, [open])
 
   return (
-    <button className='bg-[var(--menu-bg)] rounded-full text-inverted pt-[3px] pb-[2px] w-[84px] text-[14px] tracking-[-.03em]'>
-      {t('menu.button')}
-    </button>
+    <>
+      <button 
+        className='bg-[var(--menu-bg)] rounded-full text-inverted pt-[3px] pb-[2px] w-[84px] text-[14px] tracking-[-.03em]'
+        onClick={() => setOpen(!open)}
+      >
+        {t('menu.button')}
+      </button>
+      <div 
+        className={cx('fixed w-full h-full top-0 left-0 z-10 transition-blur', {
+          'pointer-events-none': !open
+        })} 
+        style={{ '--blur': open ? '20px' : '0px' } as React.CSSProperties}
+        onClick={(e) => {
+          if(e.target !== e.currentTarget) return
+          setOpen(false)
+        }}
+      >
+        <FocusTrap active={open}>
+          <div className={cx('bg-black text-white w-96 h-full right-0 absolute transition-transform duration-500 menu-bg flex flex-col gap-4 p-8 font-display text-4xl font-bold', {
+            'transform translate-x-full': !open,
+            'transform-none': open
+          })}>
+            <CloseMenuButton
+              onClose={() => setOpen(false)}
+            />
+            <Link href='https://hloth.dev/me' className='hover:text-neutral-300 transition-all hover:tracking-wide'>
+              {t('menu.about_me')}
+            </Link>
+            <Link href='https://hloth.dev/portfolio' className='hover:text-neutral-300 transition-all hover:tracking-wide'>
+              {t('menu.my_portfolio')}
+            </Link>
+            <div className='mt-auto flex items-center justify-between text-sm text-gray leading-none'>
+              <span className='font-medium'>{t('menu.contact')}:</span>
+              <span className='font-normal'>hi@hloth.dev</span>
+            </div>
+          </div>
+        </FocusTrap>
+      </div>
+    </>
   )
 }
