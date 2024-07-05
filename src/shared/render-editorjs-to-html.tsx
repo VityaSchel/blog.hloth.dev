e7fac8e1c2743d93db20500a3f763b411d38c544
VityaSchel <59040542+VityaSchel@users.noreply.github.com>
2024-07-05 06:29:12 +0400

    Add NEXT_PUBLIC_STORAGE_URL

--- src/shared/render-editorjs-to-html.tsx
+++ src/shared/render-editorjs-to-html.tsx
@@ -29,7 +29,7 @@ const ImageRenderer = (block: OutputBlockData): React.ReactNode => {
           'shadow-xl w-fit f-fit': withBackground
         })} style={{ aspectRatio: file.width / file.height }}>
           <Image 
-            src={file.url}
+            src={process.env.NEXT_PUBLIC_STORAGE_URL + file.url}
             placeholder='blur'
             blurDataURL={file.placeholder}
             alt={caption}
