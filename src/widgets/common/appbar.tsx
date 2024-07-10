de6d6e32e7d23da8043152fd4400d43d0491d768
VityaSchel <59040542+VityaSchel@users.noreply.github.com>
2024-07-10 14:22:40 +0400

    Change <a> to <Link>

--- src/widgets/common/appbar.tsx
+++ src/widgets/common/appbar.tsx
@@ -3,6 +3,7 @@ import { LanguageSwitch } from '@/features/language-switch'
 import { ThemeSwitch } from '@/features/theme-switcher'
 import { Menu } from '@/features/menu'
 import { formatTitle } from '@/shared/title'
+import Link from 'next/link'
 
 export function AppBar({ previous, next }: {
   previous?: { title: string, path: string },
@@ -19,26 +20,26 @@ export function AppBar({ previous, next }: {
           })}>
             {previous && (
               <li>
-                <a href={previous.path} className='flex items-center gap-2'>
+                <Link href={previous.path} className='flex items-center gap-2'>
                   <svg width='20' viewBox='0 0 100 60' xmlns='http://www.w3.org/2000/svg'>
                     <line x1='10' y1='50' x2='90' y2='50' stroke='currentColor' strokeWidth='6' />
                     <line x1='10' y1='50' x2='30' y2='30' stroke='currentColor' strokeWidth='6' />
                   </svg>
                   
                   {formatTitle(previous.title).title}
-                </a>
+                </Link>
               </li>
             )}
             {next && (
               <li className='self-end'>
-                <a href={next.path} className='flex items-center gap-2'>
+                <Link href={next.path} className='flex items-center gap-2'>
                   {formatTitle(next.title).title}
 
                   <svg width='20' viewBox='0 0 100 60' xmlns='http://www.w3.org/2000/svg'>
                     <line x1='10' y1='50' x2='90' y2='50' stroke='currentColor' strokeWidth='6' />
                     <line x1='70' y1='30' x2='90' y2='50' stroke='currentColor' strokeWidth='6' />
                   </svg>
-                </a>
+                </Link>
               </li>
             )}
           </ul>
