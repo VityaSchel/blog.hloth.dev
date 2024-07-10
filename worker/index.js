10745156e5f8592bb6313ef8ff51f9e4348e5a7d
VityaSchel <59040542+VityaSchel@users.noreply.github.com>
2024-07-10 17:39:16 +0400

    Remove focusing

--- worker/index.js
+++ worker/index.js
@@ -15,18 +15,5 @@ self.addEventListener('push', function (event) {
 
 self.addEventListener('notificationclick', function (event) {
   event.notification.close()
-  event.waitUntil(
-    clients.matchAll({ type: 'window', includeUncontrolled: true }).then(function (clientList) {
-      if (clientList.length > 0) {
-        let client = clientList[0]
-        for (let i = 0; i < clientList.length; i++) {
-          if (clientList[i].focused) {
-            client = clientList[i]
-          }
-        }
-        return client.focus()
-      }
-      return clients.openWindow(event.notification.data.url)
-    })
-  )
+  clients.openWindow(event.notification.data.url)
 })
