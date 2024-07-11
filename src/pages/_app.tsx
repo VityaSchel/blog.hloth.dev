3104ac464184031981a63473ff3520fa4531fdfe
VityaSchel <59040542+VityaSchel@users.noreply.github.com>
2024-07-12 02:33:28 +0400

    Fix onResize handler when route changes

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -5,16 +5,23 @@ import type { AppProps } from 'next/app'
 import { appWithTranslation } from 'next-i18next'
 import { Footer } from '@/widgets/common/footer'
 import { ThemeProvider } from 'next-themes'
+import { useRouter } from 'next/router'
 
 function HlothBlog({ Component, pageProps }: AppProps) {
+  const router = useRouter()
+  
   React.useEffect(() => {
     const onResize = () => {
       document.documentElement.style.setProperty('--page-height', `${window.document.body.scrollHeight - window.innerHeight}px`)
     }
     window.addEventListener('resize', onResize)
+    router.events.on('routeChangeComplete', onResize)
     onResize()
-    return () => window.removeEventListener('resize', onResize)
-  }, [])
+    return () => {
+      window.removeEventListener('resize', onResize)
+      router.events.off('routeChangeComplete', onResize)
+    }
+  }, [router])
 
   return (
     <ThemeProvider>
