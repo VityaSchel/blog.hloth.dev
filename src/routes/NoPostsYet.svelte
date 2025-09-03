40b7c678afae562b8b2caaabbb7caf9618b571ff
Viktor Shchelochkov <hi@hloth.dev>
2025-09-03 22:47:56 +0200

    Only show "show all posts" button when a category is selected

--- src/routes/NoPostsYet.svelte
+++ src/routes/NoPostsYet.svelte
@@ -1,11 +1,17 @@
+<script>
+	import { page } from '$app/state';
+</script>
+
 <div
 	class="
-	flex h-64 w-full flex-col items-center justify-center gap-4 text-[24.5px]
-"
+		flex h-64 w-full flex-col items-center justify-center gap-4 text-[24.5px]
+	"
 >
 	<span class="font-text tracking-normal">No posts yet</span>
 	<img src="/no.gif" alt="🙂‍↔️" />
-	<a href="/#posts" class="rounded-full border px-4 text-base font-medium">
-		Show all posts
-	</a>
+	{#if page.url.searchParams.has('category')}
+		<a href="/#posts" class="rounded-full border px-4 text-base font-medium">
+			Show all posts
+		</a>
+	{/if}
 </div>
