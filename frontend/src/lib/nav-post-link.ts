24103b4efdf61d55cb8f2abb8059e09fa0a4b8d4
Viktor Shchelochkov <hi@hloth.dev>
2025-10-22 19:05:37 +0200

    Add bottom post nav

---
+++ frontend/src/lib/nav-post-link.ts
@@ -0,0 +1,13 @@
+export type NavPostLink = { path: string; title: string; ru?: boolean };
+
+export function mapCollectionEntryToNavPostLink(
+	entry?: import("astro:content").CollectionEntry<"blog">,
+): NavPostLink | undefined {
+	return (
+		entry && {
+			path: entry.id,
+			title: entry.data.title,
+			ru: entry.data.locale === "ru",
+		}
+	);
+}
