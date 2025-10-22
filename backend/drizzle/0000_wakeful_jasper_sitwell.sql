9efc2d7313f66626bc01addb61187150b0ad00ec
Viktor Shchelochkov <hi@hloth.dev>
2025-10-22 21:41:08 +0200

    Reset backend db schema

--- backend/drizzle/0000_wakeful_jasper_sitwell.sql
+++ backend/drizzle/0000_wakeful_jasper_sitwell.sql
@@ -1,8 +1,15 @@
+CREATE TABLE "new_post_notifications" (
+	"post_id" text PRIMARY KEY NOT NULL,
+	"sent" integer DEFAULT 0 NOT NULL,
+	CONSTRAINT "new_post_notifications_post_id_unique" UNIQUE("post_id")
+);
+--> statement-breakpoint
 CREATE TABLE "push_subscriptions" (
 	"endpoint" text PRIMARY KEY NOT NULL,
 	"p256dh" text NOT NULL,
 	"auth" text NOT NULL,
-	"expires_at" timestamp with time zone
+	"expires_at" timestamp with time zone,
+	CONSTRAINT "push_subscriptions_endpoint_unique" UNIQUE("endpoint")
 );
 --> statement-breakpoint
 CREATE TABLE "reaction_challenge_solutions" (
@@ -16,7 +23,7 @@ CREATE TABLE "reaction_challenges" (
 );
 --> statement-breakpoint
 CREATE TABLE "reactions" (
-	"post_id" text NOT NULL,
+	"post_id" text PRIMARY KEY NOT NULL,
 	"👍" integer DEFAULT 0 NOT NULL,
 	"❤️" integer DEFAULT 0 NOT NULL,
 	"🔥" integer DEFAULT 0 NOT NULL,
@@ -34,4 +41,17 @@ CREATE TABLE "reactions" (
 	CONSTRAINT "reactions_post_id_unique" UNIQUE("post_id")
 );
 --> statement-breakpoint
-CREATE INDEX "reaction_challenges_client_id_created_at_idx" ON "reaction_challenges" USING btree ("client_id","created_at" DESC NULLS LAST);
+CREATE TABLE "sent_notifications" (
+	"endpoint" text NOT NULL,
+	"post_id" text NOT NULL,
+	CONSTRAINT "sent_notifications_endpoint_post_id_pk" PRIMARY KEY("endpoint","post_id")
+);
+--> statement-breakpoint
+CREATE TABLE "views" (
+	"post_id" text NOT NULL,
+	"client_id" text NOT NULL,
+	CONSTRAINT "views_client_id_unique" UNIQUE("client_id")
+);
+--> statement-breakpoint
+CREATE INDEX "reaction_challenges_client_id_created_at_idx" ON "reaction_challenges" USING btree ("client_id","created_at" DESC NULLS LAST);--> statement-breakpoint
+CREATE INDEX "views_post_id_idx" ON "views" USING btree ("post_id");
--- backend/drizzle/0003_wet_meteorite.sql
+++
@@ -1,4 +0,0 @@
-ALTER TABLE "new_post_notifications" ADD PRIMARY KEY ("post_id");--> statement-breakpoint
-ALTER TABLE "reactions" ADD PRIMARY KEY ("post_id");--> statement-breakpoint
-ALTER TABLE "new_post_notifications" ADD CONSTRAINT "new_post_notifications_post_id_unique" UNIQUE("post_id");--> statement-breakpoint
-ALTER TABLE "push_subscriptions" ADD CONSTRAINT "push_subscriptions_endpoint_unique" UNIQUE("endpoint");
