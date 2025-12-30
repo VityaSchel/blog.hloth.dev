b0516e52f161499b42ccbe57ec0f2ba295e60407
Viktor Shchelochkov <hi@hloth.dev>
2025-12-30 21:51:13 +0100

    Fix carousels placeholders

--- frontend/src/lib/get-image-as-buffer.ts
+++ frontend/src/lib/get-image-as-buffer.ts
@@ -18,9 +18,15 @@ export async function getLqip(src: string, { origin }: { origin: string }) {
 		const arrayBuffer = await req.arrayBuffer();
 		hqImgBuffer = Buffer.from(arrayBuffer);
 	}
-	let lqImg = sharp(hqImgBuffer)
-		.autoOrient()
-		.resize({ width: 24, fit: "contain", withoutEnlargement: true });
+	const hqImg = sharp(hqImgBuffer).autoOrient();
+	const meta = await hqImg.metadata();
+	const width = meta.width;
+	const height = meta.height;
+	let lqImg = hqImg.resize({
+		width: 24,
+		fit: "contain",
+		withoutEnlargement: true,
+	});
 	const useAvif = import.meta.env.PROD;
 	if (useAvif) {
 		lqImg = lqImg.avif({ quality: 50 });
@@ -28,10 +34,13 @@ export async function getLqip(src: string, { origin }: { origin: string }) {
 		lqImg = lqImg.jpeg({ quality: 1 });
 	}
 	const lqImgBuffer = await lqImg.toBuffer();
-	return (
-		"data:image/" +
-		(useAvif ? "avif" : "jpeg") +
-		";base64," +
-		lqImgBuffer.toString("base64")
-	);
+	return {
+		src:
+			"data:image/" +
+			(useAvif ? "avif" : "jpeg") +
+			";base64," +
+			lqImgBuffer.toString("base64"),
+		width,
+		height,
+	};
 }
